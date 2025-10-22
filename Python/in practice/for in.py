89f4288915caeac042e7cc5e53d73a020dbd7c21
REUBENJ2025 <reubencoder@gmail.com>
2025-10-22 10:22:26 +0100

    added random passowrd generator 	modified:   Python/in practice/for in.py

--- Python/in practice/for in.py
+++ Python/in practice/for in.py
@@ -3,7 +3,7 @@
 #22/10/25
 #for in
 #Computer science
-
+import random, string
 nlist = [10, 20, 30, 40]
 
 for x in range(0, len(nlist)):
@@ -19,3 +19,23 @@ print(' ')
 
 if num in nlist:
     print("Found")
+
+while num in nlist:
+    print("still repeating")
+    nlist.remove(20)
+
+new_list = []
+
+for i in range(100000):
+    new_list.append(1 + i)
+
+#print(new_list)
+
+character = string.ascii_letters + string.digits + string.punctuation + string.
+
+
+
+password = [random.choice(character) for x in range(0,200)]
+
+
+print(' '.join(password))
