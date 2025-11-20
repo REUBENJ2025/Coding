945e829c8a64e727f7b3bff63de577a5706686b0
ReubenJ <139593819+REUBENJ2025@users.noreply.github.com>
2025-11-20 13:47:07 +0000

    Bubble sort while loop

--- Python/bubble sort.py
+++ Python/bubble sort.py
@@ -3,27 +3,26 @@
 #Computer Science
 #Greg Allcock
 #Bubble sort
-import time
 
-start_time = time.time()
 
 my_list = [23,41,11,17,34,56]
 
-print(my_list)
+n = len(my_list)-1
+swapped = True
 
-n =  len(my_list) -1
-for x in range(0,n):
+while(swapped):
+    swapped = False
+
     for index in range(0,n):
-        if (my_list[index] > my_list[index+1]):
+        if(my_list[index] > my_list[index+1]):
             temp = my_list[index]
             my_list[index] = my_list[index+1]
             my_list[index+1] = temp
 
-print(my_list)
-print(f"The length of the list is {n}")
+            swapped = True
 
-end_time = time.time()-start_time
-print(end_time)
+
+print(my_list)
 
 
 
